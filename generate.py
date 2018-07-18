4b45ebe1ddbe633435c3e396d5309afcf7002c13
Ben Boyter <ben@boyter.org>
2018-07-18 15:16:19 +1000

    Add parties and clan generation

--- generate.py
+++ generate.py
@@ -20,12 +20,13 @@ def random_name(male=True):
         name = random.choice(first) + random.choice(middle) + random.choice(last)
     return name.capitalize()
 
+
 def house_name():
     length = random.randint(2, 3)
 
-    first = ['a', 'min', 'xu', 'ana', 'lu', 'ke', 'tsu', 'du', 'xa', 'ko', 'cho', 'io', 'shon', 'ton', 'shin', 'tus', 'tu', 'ek', 'da', 'in']
+    first = ['a', 'min', 'xu', 'ana', 'lu', 'ke', 'tsu', 'du', 'xa', 'ko', 'cho', 'io', 'shon', 'ton', 'shin', 'tus', 'tu', 'ek', 'da', 'in', 'xo']
     middle = ['co', 'wa', 'ra', 'sa', 'le', 'sta', 'cat', 'cha', 'ca', 'na', 'sho', 'mar', 'za', 'cal', 'am', 'chin', 'rod']
-    last = ['ma', 'nabi', 'tes', 'ti', 'jun', 'wan', 'ni', 'ri', 'ecas', 'tai', 'pan', 'da', 'la', 'mi', 'gu', 'wai', 'ora', 'chi', 'do', 'aka']
+    last = ['ma', 'nabi', 'tes', 'ti', 'jun', 'wan', 'ni', 'ri', 'ecas', 'tai', 'pan', 'da', 'la', 'mi', 'gu', 'wai', 'ora', 'chi', 'do', 'aka', 'ka', 'tai', 'ara', 'can', 'kel', 'ta', 'kla', 'sai']
 
     name = ''
     if length == 2:
@@ -35,13 +36,40 @@ def house_name():
     return name.capitalize()
 
 
+def clan_name():
+    length = random.randint(2, 3)
+
+    first = ['kana', 'ome', 'shon', 'io', 'xa', 'ha']
+    middle = ['za', 'na', 'ca', 'da', 'ja']
+    last = ['wai', 'chan', 'shoni', 'ni', 'la', 'ma']
+
+    name = ''
+    if length == 2:
+        name = random.choice(first) + random.choice(last)
+    if length == 3:
+        name = random.choice(first) + random.choice(middle) + random.choice(last)
+    return name.capitalize()
+
+
+def party_name():
+    return random.choice([
+        'Party for Progress',
+        'Blue Wheel Party',
+        'Yellow Flower Party',
+        'War Party',
+        'Jade Eye Party',
+        'Silver Party',
+        'New Party',
+        'Summer Breeze Party'
+    ])
 
 if __name__ == "__main__":
-    for x in range(10):
-        male = random.randint(0, 10) <= 8
+    for x in range(100000):
+        male = random.randint(0, 1) == 1
 
         prefix = 'Lady '
         if male:
             prefix = 'Lord '
 
-        print prefix + random_name(male=male) + ' of the ' + house_name()
+        print prefix + random_name(male=male) + ' of the ' + house_name() + ' and clan ' + clan_name() + ' and ' + party_name()
+    
